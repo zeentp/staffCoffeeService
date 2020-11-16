3396f108cf5805264e21649bf3cb463ab41c26dc
zeentp <nuthaporn.w@ku.th>
2020-11-16 18:39:04 +0700

    add loginPage

--- src/loginPage.js
+++ src/loginPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './css/loginPage.css';
 import 'antd/dist/antd.css';
+import './css/loginPage.css';
 import firebase, { auth, provider } from './firebase.js';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox, Card} from 'antd';
 
 
 const onFinish = (values) => {
@@ -25,39 +25,41 @@ class loginPage extends React.Component {
   render() {
     return (
       <div class="bg">
-      <Form 
-        name="basic" initialValues={{ remember: true,}}  onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Form.Item class=""
-          label="Username"
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your username!',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+        <Card title="Coffee Shop" style={{ width: 500}} class="body">
+          <Form
+            name="basic" initialValues={{ remember: true, }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item >
-          <Button type="primary" htmlType="submit">
-            Submit
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item >
+              <Button type="primary" htmlType="submit">
+                Submit
           </Button>
-        </Form.Item>
-      </Form>
+            </Form.Item>
+          </Form>
+        </Card>
       </div>
     );
   }
